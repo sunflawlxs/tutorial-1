93cc2c64b6db5d63c863f94ae02f091d34dda07c
sunflawlxs <sherylivana99@gmail.com>
2024-02-24 15:47:34 +0700

    try to merge

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomePageController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomePageController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-public class HomePageController {
+public class HomepageController {
     @GetMapping("/")
     public String homepage() {
         return "homepage";
