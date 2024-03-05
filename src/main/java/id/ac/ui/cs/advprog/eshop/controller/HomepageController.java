44763bf0db292a97635c100d09c488d5cee6318e
sunflawlxs <sherylivana99@gmail.com>
2024-03-05 10:52:15 +0700

    [RED] Add tests for Order model

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomepageController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomepageController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-public class HomePageController {
+class HomePageController {
 
     @GetMapping("/")
     public String homepage() {
